a8c9f5ba46bfc70fb45cbcbbf74f141059d703b1
Anna Kovalchuk <annazukh@gmail.com>
2022-09-05 18:57:29 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,7 +3,7 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 import SimpleLightbox from 'simplelightbox';
-// Додатковий імпорт стилів
+// Додатков. імпорт стилів
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const gallery = document.querySelector('.gallery');
