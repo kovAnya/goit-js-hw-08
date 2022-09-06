afe230e85531cbc7b0f67577f376cb6463227555
Anna Kovalchuk <annazukh@gmail.com>
2022-09-06 15:30:44 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,12 +3,15 @@ import Player from '@vimeo/player';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 const throttle = require('lodash.throttle');
+const currentTime = JSON.parse(
+  localStorage.getItem('videoplayer-current-time')
+);
+
+if (currentTime) {
+  player.setCurrentTime(currentTime.seconds);
+}
 
 player.on('timeupdate', throttle(onPlay, 1000));
-
-player.setCurrentTime(
-  JSON.parse(localStorage.getItem('videoplayer-current-time')).seconds
-);
 
 function onPlay(event) {
   localStorage.setItem('videoplayer-current-time', JSON.stringify(event));
